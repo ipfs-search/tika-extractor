79f5937f0749f7c4a80b1c79f7738a6f85588187
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-12-11 21:44:12 +0000

    Cleaner exception handling.

--- src/main/java/org/ipfs_search/tika_extractor/ExtractorResource.java
+++ src/main/java/org/ipfs_search/tika_extractor/ExtractorResource.java
@@ -2,6 +2,7 @@ package org.ipfs_search.tika_extractor;
 
 import java.net.URL;
 import java.net.MalformedURLException;
+import java.io.IOException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,25 +10,46 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.ServiceUnavailableException;
 
 import javax.inject.Inject;
 
+import org.apache.tika.exception.TikaException;
+import org.xml.sax.SAXException;
+
+import org.jboss.logging.Logger;
+
 @Path("/extract")
 public class ExtractorResource {
+    private static final Logger LOG = Logger.getLogger(ExtractorResource.class);
 
     @Inject
     ExtractorService service;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String extract(@QueryParam("url") String urlStr) throws Exception {
+    public String extract(@QueryParam("url") String urlStr) throws WebApplicationException {
 		URL url;
     	try {
 	    	url = new URL(urlStr);
     	} catch (MalformedURLException e) {
+    		LOG.error("Invalid URL", e);
     		throw new BadRequestException(e);
     	}
 
-        return service.extract(url);
+    	String result;
+        try {
+			result = service.extract(url);
+        } catch (IOException e) {
+        	// Upstream unavailable. Tell clients to try again in 2s.
+        	throw new ServiceUnavailableException(2L, e);
+        } catch (TikaException | SAXException e) {
+    		LOG.error(e.toString(), e);
+        	throw new InternalServerErrorException(e);
+        }
+
+        return result;
     }
 }
