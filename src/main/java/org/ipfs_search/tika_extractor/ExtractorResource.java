1079ae782ad98066ba1c78b14f11f536db667e69
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-12-13 14:28:21 +0000

    Use reactive route; process resource in eventloop.

--- src/main/java/org/ipfs_search/tika_extractor/ExtractorResource.java
+++ src/main/java/org/ipfs_search/tika_extractor/ExtractorResource.java
@@ -5,11 +5,16 @@ import java.net.MalformedURLException;
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import io.quarkus.vertx.web.Route;
+import io.quarkus.vertx.web.Param;
+import io.smallrye.mutiny.Uni;
+
+// import javax.ws.rs.GET;
+// import javax.ws.rs.Path;
+// import javax.ws.rs.QueryParam;
+// import javax.ws.rs.Produces;
+// import javax.ws.rs.core.MediaType;
+
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.InternalServerErrorException;
@@ -22,39 +27,45 @@ import org.xml.sax.SAXException;
 
 import org.jboss.logging.Logger;
 
-@Path("/extract")
+// @Path("/extract")
 public class ExtractorResource {
     private static final Logger LOG = Logger.getLogger(ExtractorResource.class);
 
     @Inject
     ExtractorService service;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public CompletionStage<String> extract(@QueryParam("url") String urlStr) throws WebApplicationException {
-		URL url;
-    	try {
-	    	url = new URL(urlStr);
-    	} catch (MalformedURLException e) {
-    		LOG.error("Invalid URL", e);
-    		throw new BadRequestException(e);
-    	}
+    // @GET
+    // @Produces(MediaType.APPLICATION_JSON)
+    @Route(path="/extract")
+    public Uni<String> extract(@Param("url") String urlStr) throws WebApplicationException {
+        URL url;
+        try {
+            url = new URL(urlStr);
+        } catch (MalformedURLException e) {
+            LOG.error("Invalid URL", e);
+            throw new BadRequestException(e);
+        }
 
-        return service.extract(url);
+        Uni<String> result = Uni.createFrom().completionStage(service.extract(url));
+
+        LOG.info("Returning async response.");
+
+        return result;
+
 
         // TODO: Re-introduce nuanced error handling, separating:
         // 1. Upstream timeouts
         // 2. Parser errors
         // 3. Upstream unavailable
         // 4. Body content read limit overschrijding
-   //      try {
-			// return service.extract(url);
-   //      } catch (IOException e) {
-   //      	// Upstream unavailable. Tell clients to try again in 2s.
-   //      	throw new ServiceUnavailableException(2L, e);
-   //      } catch (TikaException | SAXException e) {
-   //  		LOG.error(e.toString(), e);
-   //      	throw new InternalServerErrorException(e);
-   //      }
+       //      try {
+                // return service.extract(url);
+       //      } catch (IOException e) {
+       //       // Upstream unavailable. Tell clients to try again in 2s.
+       //       throw new ServiceUnavailableException(2L, e);
+       //      } catch (TikaException | SAXException e) {
+       //       LOG.error(e.toString(), e);
+       //       throw new InternalServerErrorException(e);
+       //      }
     }
 }
